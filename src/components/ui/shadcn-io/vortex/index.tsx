c952cddf3fefab3465be439c94014b0a0c62c64a
Jesus Rodriguez-Luna <112538272+JRodriguez-Luna@users.noreply.github.com>
2025-10-15 11:42:17 -0700

    minor issue #8

--- src/components/ui/shadcn-io/vortex/index.tsx
+++ src/components/ui/shadcn-io/vortex/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-const */
 'use client';
 
 import { cn } from "../../../../lib/utils";
@@ -6,6 +7,7 @@ import { createNoise3D } from "simplex-noise";
 import { motion } from "framer-motion";
 
 interface VortexProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   children?: any;
   className?: string;
   containerClassName?: string;
